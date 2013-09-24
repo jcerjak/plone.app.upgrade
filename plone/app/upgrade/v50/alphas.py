a884f48ea7d13285b06ee2228683a25f8dc2686b
Nathan Van Gheem <vangheem@gmail.com>
2013-09-24 13:23:09 -0500

    add keyring upgrades

--- plone/app/upgrade/v50/alphas.py
+++ plone/app/upgrade/v50/alphas.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-import logging
-
 from Acquisition import aq_parent, aq_base
-from Products.CMFCore.utils import getToolByName
-from plone.app.upgrade.utils import loadMigrationProfile
 from plone.keyring.interfaces import IKeyManager
 from plone.keyring.keyring import Keyring
 from plone.keyring.keymanager import KeyManager
 from zope.component import getUtility
 from zope.component import getSiteManager
 from zope.component.hooks import getSite
+from Products.CMFCore.utils import getToolByName
+from plone.app.upgrade.utils import loadMigrationProfile
 from plone.app.upgrade.v40.alphas import cleanUpToolRegistry
 import logging
 
@@ -74,6 +72,8 @@ def to50alpha1(context):
         if not qi.isProductInstalled('plonetheme.barceloneta'):
             qi.installProduct('plonetheme.barceloneta')
 
+    upgrade_keyring(context)
+
 
 def lowercase_email_login(context):
     """If email is used as login name, lowercase the login names.
@@ -110,3 +110,24 @@ def migrate_members_default_view(portal):
     if 'index_html' in members:
         del members['index_html']
     members.layout = '@@member-search'
+
+
+def upgrade_keyring(context):
+    logger.info('upgrading keyring')
+    manager = getUtility(IKeyManager)
+
+    manager[u'_system'].fill()
+
+    manager[u'_anon'] = Keyring()
+    manager[u'_anon'].fill()
+
+    manager[u'_forms'] = Keyring()
+    manager[u'_forms'].fill()
+
+    logger.info('add keyring to zope root if not done already')
+    app = aq_parent(getSite())
+    sm = getSiteManager(app)
+
+    if sm.queryUtility(IKeyManager) is None:
+        obj = KeyManager()
+        sm.registerUtility(aq_base(obj), IKeyManager, '')
